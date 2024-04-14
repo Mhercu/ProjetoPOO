a85959cc04f534e3e3b214235afbc7724cbd4370
Maria Herculana <112400121+Mhercu@users.noreply.github.com>
2024-04-14 15:20:58 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,3 +1,54 @@
 public class Produto {
+    private int idProduto;
+    private String nomeProduto;
+    private String descricaoProduto;
+    private float precoProduto;
+    
+    public int getIdProduto() {
+    return idProduto;
+}
 
+public void setIdProduto(int idProduto) {
+    this.idProduto = idProduto;
+}
+
+public String getNomeProduto() {
+    return nomeProduto;
+}
+
+public void setNomeProduto(String nomeProduto) {
+    this.nomeProduto = nomeProduto;
+}
+
+public String getDescricaoProduto() {
+    return descricaoProduto;
+}
+
+public void setDescricaoProduto(String descricaoProduto) {
+    this.descricaoProduto = descricaoProduto;
+}
+
+public float getPrecoProduto() {
+    return precoProduto;
+}
+
+public void setPrecoProduto(float precoProduto) {
+    this.precoProduto = precoProduto;
+}
+
+    public void adicionarProduto(){
+    return;
+}
+    public void removerProduto(){
+    return;
+}
+    public void visualizarProduto(){
+    return;
+}
+    public void editarProduto(){
+    return;
+}
+    public void listarProduto(){
+    return;
+}
 }
